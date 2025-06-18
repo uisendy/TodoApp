1d149f883ec3916c5114d9ce187539d69319e285
uisendy <ini.e.sendy@gmail.com>
2025-06-19 00:31:27 +0100

    [dev]: Add: Interfaces

---
+++ TodoAppApi/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using TodoAppApi.DTOs.Common;
+
+namespace TodoAppApi.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled Exception");
+
+                context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/json";
+
+                var response = ApiResponseDto<string>.Error("An unexpected error occurred");
+
+                var json = JsonSerializer.Serialize(response);
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+
+}
