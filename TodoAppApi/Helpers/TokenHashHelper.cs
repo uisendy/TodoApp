a9d70418310f26bb99697dd3662cab9fff329e28
uisendy <ini.e.sendy@gmail.com>
2025-06-19 00:27:52 +0100

    [dev]: Add: Token hashing Helper

---
+++ TodoAppApi/Helpers/TokenHashHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace TodoAppApi.Helpers
+{
+    public static class TokenHashHelper
+    {
+        public static string HashToken(string token)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(token);
+        }
+
+        public static bool VerifyToken(string rawToken, string hashedToken)
+        {
+            return BCrypt.Net.BCrypt.Verify(rawToken, hashedToken);
+        }
+    }
+}
