49eb9e5e0a5929975091b703557722999cfd0383
uisendy <ini.e.sendy@gmail.com>
2025-06-19 00:30:35 +0100

    [dev]: Add: ClaimPrincipalExtensions => GetUserByTokenId

---
+++ TodoAppApi/Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace TodoAppApi.Infrastructure.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static Guid GetUserId(this ClaimsPrincipal user)
+        {
+            var sub = user.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+            return sub != null && Guid.TryParse(sub, out var userId) ? userId : throw new UnauthorizedAccessException("Unauthorized");
+        }
+    }
+}
