68454a418d0d10bedcdeb0da427bb9a15fb12c46
uisendy <ini.e.sendy@gmail.com>
2025-06-19 00:24:27 +0100

    [dev]: Add: AppDbContext Factory

---
+++ TodoAppApi/Data/AppDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace TodoAppApi.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            if (string.IsNullOrEmpty(connectionString))
+                throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
