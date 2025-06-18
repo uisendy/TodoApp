14c6aecf93edf86481053ddc512bc9132edb02b6
uisendy <ini.e.sendy@gmail.com>
2025-06-19 00:22:19 +0100

    [dev]: Add: EmailServices

---
+++ TodoAppApi/Services/EmailService.cs
@@ -0,0 +1,14 @@
+﻿using TodoAppApi.Interfaces;
+
+namespace TodoAppApi.Services
+{
+    public class EmailService : IEmailService
+    {
+        public Task SendEmailOtpAsync(string email, string otp)
+        {
+            Console.WriteLine($"Sending OTP {otp} to {email}");
+            return Task.CompletedTask;
+        }
+    }
+
+}
