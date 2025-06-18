1c26ae39d13ae436cf37a9c71b45cec7f2711b49
uisendy <ini.e.sendy@gmail.com>
2025-06-19 00:24:09 +0100

    [dev]: Add: AppDbContext

---
+++ TodoAppApi/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoAppApi.Entities;
+
+namespace TodoAppApi.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Todo> Todos => Set<Todo>();
+        public DbSet<Tag> Tags => Set<Tag>();
+        public DbSet<TodoTag> TodoTags => Set<TodoTag>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoTag>()
+                .HasKey(tt => new { tt.TodoId, tt.TagId });
+
+            modelBuilder.Entity<TodoTag>()
+                .HasOne(tt => tt.Todo)
+                .WithMany(t => t.TodoTags)
+                .HasForeignKey(tt => tt.TodoId);
+
+            modelBuilder.Entity<TodoTag>()
+                .HasOne(tt => tt.Tag)
+                .WithMany(t => t.TodoTags)
+                .HasForeignKey(tt => tt.TagId);
+        }
+    }
+
+}
