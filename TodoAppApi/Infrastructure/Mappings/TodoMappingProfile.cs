29b4eefccceaa176114567f2225c3421778a8f66
uisendy <ini.e.sendy@gmail.com>
2025-06-19 00:29:06 +0100

    [dev]: Add: Maps

---
+++ TodoAppApi/Infrastructure/Mappings/TodoMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TodoAppApi.DTOs.Todos;
+using TodoAppApi.Entities;
+
+namespace TodoAppApi.Infrastructure.Mappings
+{
+    public class TodoMappingProfile : Profile
+    {
+        public TodoMappingProfile()
+        {
+            CreateMap<Todo, TodoResponseDto>()
+                .ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.TodoTags.Select(tt => tt.Tag.Name)))
+                .ReverseMap();
+
+            CreateMap<TodoRequestDto, Todo>();
+        }
+    }
+}
