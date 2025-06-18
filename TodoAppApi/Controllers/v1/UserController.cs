1471fed4b0437ac5ffd0f6a05bbc8f39def0338a
uisendy <ini.e.sendy@gmail.com>
2025-06-19 00:23:12 +0100

    [dev]: Add: UserController

---
+++ TodoAppApi/Controllers/v1/UserController.cs
@@ -0,0 +1,47 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TodoAppApi.DTOs.Common;
+using TodoAppApi.DTOs.Todos;
+using TodoAppApi.DTOs.Users;
+using TodoAppApi.Infrastructure.Extensions;
+using TodoAppApi.Interfaces;
+
+namespace TodoAppApi.Controllers.v1
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/users")]
+    [ApiVersion("1.0")]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("current-user")]
+        [Authorize]
+        public async Task<IActionResult> GetCurrentUser()
+        {
+            try
+            {
+                var userId = User.GetUserId();
+                var userProfile = await _userService.GetUserProfileAsync(userId);
+                return Ok(ApiResponseDto<UserResponseDto>.Success(userProfile));
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(ApiResponseDto<string>.Error(ex.Message));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ApiResponseDto<string>.Error("An unexpected error occurred."));
+            }
+        }
+
+    }
+}
